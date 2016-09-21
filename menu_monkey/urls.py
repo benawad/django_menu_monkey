36c5afe050db8a88d96a7d0d2cafff707bdeb46e
ben awad <benawad97@gmail.com>
2016-09-21 17:30:58 -0500

    redirect home page to recipe page

--- menu_monkey/urls.py
+++ menu_monkey/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/recipes/')),
     url(r'^recipes/', include('recipes.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^groceries/', include('groceries.urls')),
